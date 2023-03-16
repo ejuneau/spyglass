90b21d3ff31f91630d7976a413030088b9bd7e38
ejuneau <rcjuneau8@gmail.com>
2023-03-16 11:11:19 -0400

    Implemented emailjs to provide funcitonality for './Contact

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { send } from 'emailjs-com';
+import './ContactPage.css';
 
 export default function ContactPage(props) {
+
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+      });
+    
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+          'service_5ujuq5p',
+          'template_2huvbwp',
+          toSend,
+          '3p82pPQGGw838DKuu'
+        )
+          .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+          })
+          .catch((err) => {
+            console.log('FAILED...', err);
+          });
+      };
+    
+      const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
+
     return (
         <div className="ContactPageComponent">
             <h1>Like what you see?</h1>
-            <form action="mailto:rcjuneau8@gmail.com" method="get" enctype="text/plain">
-                <div>
-                    <label for="name">Name:
-                        <input type="text" name="name" id="name" />
-                    </label>
-                </div>
-                <div>
-                    <label for="email">Email:
-                        <input type="text" name="email" id="email" />
-                    </label>
-                </div>
-                <div>
-                    <label>Comments:</label>
-                    <br />          
-                    <textarea name="comments" rows="12" cols="35">Send your comments to us.</textarea>
-                </div>
-                <div>
-                    <input type="submit" name="submit" value="Send" />
-                    <input type="reset" name="reset" value="Clear Form" />
-                </div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    name='from_name'
+                    placeholder='Your Name'
+                    value={toSend.from_name}
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='reply_to'
+                    placeholder='Your email'
+                    value={toSend.reply_to}
+                    onChange={handleChange}
+                />
+                <textarea
+                    type='text'
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
+                />
+
+                <button type='submit'>Submit</button>
             </form>
         </div>
     )
