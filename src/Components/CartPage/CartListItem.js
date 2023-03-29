c96c55c2c8ccd58176bb63f09265c1127047a7b3
ejuneau <rcjuneau8@gmail.com>
2023-03-29 16:40:52 -0400

    Implemented animations for empty cart

---
+++ src/Components/CartPage/CartListItem.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useDispatch, useSelector } from "react-redux";
+import Products from "../../Util/Products";
+import { useSearchParams } from "react-router-dom";
+import { modifyQuantity, removeFromCart } from "../../Util/cartSlice";
+
+export default function CartListItem(props) {
+    const searchParams = useSearchParams();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.contents);
+    const [quantity, setQuantity] = useState(1);
+    const rotateBy = useSelector((state)=> state.rotate.rotateBy);
+    const [decCounter, setDecCounter] = useState(0);
+    const [incCounter, setIncCounter] = useState(0);
+    const [decDirection, setDecDirection] = useState({initial: "1rem", exit: "-1rem"});
+
+    function increaseQuantity() {
+        setQuantity(prevNumber => prevNumber + 1);
+        setDecCounter(0);
+        setIncCounter(prevNumber => prevNumber + 1);
+    }
+    function decreaseQuantity() { 
+        props.item.quantity>1?setQuantity(props.item.quantity - 1):setQuantity(1);  
+        setIncCounter(0);
+        setDecCounter(prevNumber => prevNumber + 1);
+    }
+    useEffect(()=> {
+        setDecDirection(incCounter > decCounter ? {initial: "1rem", exit: "-1rem"} : {initial: "-1rem", exit: "1rem"});
+    }, [incCounter, decCounter]);
+    console.log(props.item);
+    return(
+    <motion.div 
+    key={`CartListItem ${props.item.id}-${props.item.variant}`} 
+    
+    className="cartListItem"
+    
+    layout 
+    initial={{opacity: 0}}
+    animate={{opacity: 1}} 
+    exit={{opacity: 0}} 
+    >
+                                <Link id="titlePic" to={`/Shop/Product/${Products.find(items => items.id === props.item.id).name}${Products.find(items => items.id === props.item.id).variants.length > 1?`?variant=${props.item.variant}`:''}`} >
+                                    <h1>The {Products.find(items => items.id === props.item.id).name}</h1>
+                                    <img id="cartImg" src={Products.find(items => items.id === props.item.id).variants[props.item.variant].photos.front} alt={`The ${Products.find(items => items.id === props.item.id).name}, seen from the front`} />
+                                </Link>
+                                <div id="cartPageOptions">
+                                    <div className="cartPageOptionsContainer">
+                                    {/* <button id="quantButton" onClick={()=>{dispatch(modifyQuantity({id: item.id, variant: item.variant, newQuantity: item.quantity - 1}))}}>-</button>
+                                    <button id="quant">{item.quantity}</button>
+                                    <button id="quantButton" onClick={()=>{dispatch(modifyQuantity({id: item.id, variant: item.variant, newQuantity: item.quantity + 1}))}}>+</button> */}
+
+                                        <AnimatePresence mode="sync">
+
+                                            <div className="CartButtonsContainer" id="isInCart">
+                                                <button id="quantButton" onClick={()=>{decreaseQuantity(); dispatch(modifyQuantity({id: props.item.id, variant: props.item.variant, newQuantity: props.item.quantity - 1}))}}>
+                                                    <AnimatePresence mode="wait">
+                                                        <motion.p 
+                                                        layout
+                                                        key={`decQuant${props.item.quantity}IC`} 
+                                                        initial={{
+                                                            x: decDirection.initial, 
+                                                            opacity: 0, 
+                                                            transition: {duration: 0.1}
+                                                        }} 
+                                                        animate={{
+                                                            x: 0, 
+                                                            opacity: 1, 
+                                                            transition: {duration: 0.1}
+                                                        }} 
+                                                        exit={{
+                                                            x: decDirection.exit, 
+                                                            opacity: 0, 
+                                                            transition:{duration: 0.1}
+                                                            }}>-</motion.p>
+                                                    </AnimatePresence>
+                                                </button>
+                                                <AnimatePresence mode="popLayout">
+                                                    <motion.div 
+                                                    key="inCart" 
+                                                    initial={{
+                                                        opacity: 0, 
+                                                        y: "5vh"}} 
+                                                    animate={{
+                                                        opacity: 1, 
+                                                        y: 0}} 
+                                                    exit={{
+                                                        opacity: 0, 
+                                                        y: "5vh"}} 
+                                                    style={{position: "relative", display: 'flex', color:"var(--white)", fontFamily:"Noir", margin: 0, display: "flex", alignItems: "center", fontSize:"1.2rem"}}>
+                                                        <AnimatePresence>
+                                                            <motion.p 
+                                                            key={`${props.item.id}-${props.item.variant}-${props.item.quantity}`}
+                                                            initial={{
+                                                                opacity: 0, 
+                                                                y: incCounter > decCounter ? "5vh" : "-5vh",
+                                                                scaleY: 0
+                                                            }} 
+                                                                animate={{
+                                                                    opacity: 1, 
+                                                                    y: 0,
+                                                                    scaleY: 1
+                                                                }} 
+                                                                exit={{
+                                                                    opacity: 0, 
+                                                                    y: incCounter > decCounter ? "-5vh"  :"5vh",
+                                                                    scaleY: 0
+                                                                }}  
+                                                                style={{position: "absolute", margin: 0}}>{props.item.quantity}</motion.p> 
+                                                        </AnimatePresence>
+                                                        <p style={{margin: 0, marginLeft: "2rem"}}>in cart</p>
+                                                    </motion.div>
+                                                </AnimatePresence>
+                                                <button id="quantButton" onClick={()=>{increaseQuantity(); dispatch(modifyQuantity({id: props.item.id, variant: props.item.variant, newQuantity: props.item.quantity + 1}))}}>
+                                                    <motion.p
+                                                    key={`incQuant${props.item.quantity}IC`}  
+                                                    initial={{rotate: incCounter > decCounter ? "-90deg" : "90deg" }}
+                                                    animate={{rotate: 0, transition:{duration: 0.2}}}
+                                                    exit={{rotate: incCounter > decCounter ? "90deg" : "-90deg"}}>+</motion.p>
+                                                </button>
+                                            </div> 
+                                        </AnimatePresence>
+                                    </div> 
+                                    <button id="removeFromCart" onClick={()=>{dispatch(removeFromCart({id: props.item.id, variant: props.item.variant}));}}>Remove from cart</button>
+                                </div>
+                            </motion.div> 
+    )
+}
--- src/Components/CartPage/CartPage.js
+++ src/Components/CartPage/CartPage.js
@@ -6,15 +6,36 @@ import { Link } from "react-router-dom";
 import { addToCart, modifyQuantity, removeFromCart, clearCart } from "../../Util/cartSlice";
 import binos from '../../Assets/Images/binos.png';
 import NTSH from '../../Assets/Images/NTSH.png';
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Products from "../../Util/Products";
 import LWYS from '../../Assets/Images/LWYS.webp';
+import CartListItem from './CartListItem';
 
 export default function CartPage(props) {
     const cart = useSelector((state) => state.cart)
     const antiRotateBy = useSelector((state) => state.rotate.antiRotateBy);
     const rotateBy = useSelector((state) => state.rotate.rotateBy);
     const dispatch = useDispatch();
+    const animationVariants = {
+        initial: {
+          opaicty: 0,
+          transition: {
+            duration: 0.2
+          }
+        },
+        animate: {
+          opacity: 1,
+          transition: {
+            duration: 0.25
+          }
+        },
+        exit: {
+          opacity: 0,
+          transition: {
+            duration: 0.1
+          }
+        }
+      } 
 
     const  formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -23,52 +44,46 @@ export default function CartPage(props) {
     return (
         <div className="CartPageComponent">
             <div className="CartPageSpacer" />
-            {
-                    cart.contents.length > 0 && <>
+            {cart.contents.length === 0 && <motion.div id="outline" key="outline" initial={{opacity: 0, clipPath:"none"}} animate={{opacity:1, maskImage: "radial-gradient(circle, rgba(10,10,10,0) 10%, rgba(10,10,10,1) 50%, rgba(10,10,10,1) 100%)", top: ["-75vh", "0vh", "-150vh", "0vh", "-150vh", "-75vh"], left: ["-75vw", "-150vw", "-150vw", "0vw", "0vw", "-75vw"], transition: {top: {duration: 6, delay: 2, repeat: Infinity, repeatType: "loop",}, left: {duration: 6, delay: 2, repeat: Infinity, repeatType: "loop", },opacity: {duration: 0.5}, maskImage: {duration: 0.01, delay: 1}}}}></motion.div>}
+            <motion.div key="cartList" className="cartList" variants={animationVariants}
+            layout 
+            initial="initial" 
+            animate="animate" 
+            exit="exit" >
+                <AnimatePresence mode="sync">
+                {
+                    cart.contents.length > 0 && <motion.div key="LWYS" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
                     
                     <img src={LWYS} alt="like what you see?" id="LWYS" />
-                    <button id="dumpCart" onClick={()=> {dispatch(clearCart())}}>Dump entire cart</button> </>
+                    <button id="dumpCart" onClick={()=> {dispatch(clearCart())}}>Dump entire cart</button> </motion.div>
                 }
-            <div className="cartList">
                 {
-                    cart.contents.length > 0 && cart.contents.map((item) => {
-                        // console.log(item);
-                        return (
-                            <div className="cartListItem" key={`${item.id}-${item.variant}`}>
-                                <Link id="titlePic" to={`/Shop/Product/${Products.find(items => items.id === item.id).name}${Products.find(items => items.id === item.id).variants.length > 1?`?variant=${item.variant}`:''}`} >
-                                    <h1>The {Products.find(items => items.id === item.id).name}</h1>
-                                    <img id="cartImg" src={Products.find(items => items.id === item.id).variants[item.variant].photos.front} alt={`The ${Products.find(items => items.id === item.id).name}, seen from the front`} />
-                                </Link>
-                                <div id="cartPageOptions">
-                                <div className="cartPageOptionsContainer">
-                                    <button id="quantButton" onClick={()=>{dispatch(modifyQuantity({id: item.id, variant: item.variant, newQuantity: item.quantity - 1}))}}>-</button>
-                                    <button id="quant">{item.quantity}</button>
-                                    <button id="quantButton" onClick={()=>{dispatch(modifyQuantity({id: item.id, variant: item.variant, newQuantity: item.quantity + 1}))}}>+</button>
-                                </div> 
-                                <button id="removeFromCart" onClick={()=>{dispatch(removeFromCart({id: item.id}));}}>Remove from cart</button>
-                                </div>
-                            </div>
-                            
-                        )
-                    })
+                cart.contents.length > 0 && cart.contents.map((item) => {
+                    // console.log(item);
+                    return (
+                        <CartListItem key={`${item.id}-${item.variant}`} item={item}/>
+                    )
+                })
                 }
+                
+                
                 {
                     cart.contents.length === 0 &&
-                    <div className="NTSH">
+                    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} layout key="NTSH" className="NTSH">
                         <img id="NTSH" src={NTSH} style={{rotate: antiRotateBy}} alt="Nothing to see here" />
                         <motion.img  key="binos" id="binos" animate={{x: ["-20%", "20%"], y: ["0%", "15%", "0%"], rotate: ["0deg", "-20deg", "-40deg"], transition: {duration: 2, repeat: Infinity, repeatType: "mirror"}}} src={binos} alt="binoculars in search of frames" />
                         <Link  style={{rotate: rotateBy}} to="/Shop" id="emptyCartShop">Take a look</Link>
-                    </div>
+                    </motion.div>
                 }
                 {
                     cart.contents.length > 0 &&
-                    <div className="total">
+                    <motion.div key="Total" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="total">
                         <p>Total: {formatter.format(cart.total)}</p>
                         <Link id="checkoutButton" to="./Checkout">Checkout</Link>
-                    </div>
+                    </motion.div>
                 }
-            
-            </div>
+                </AnimatePresence>
+            </motion.div>
             <div className="CartPageSpacer" />
         </div>
 
